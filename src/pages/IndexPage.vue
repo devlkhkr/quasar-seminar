40cc7bb8f37ee91cbd6006e4272447b9f41f23d2
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2023-09-06 09:24:20 +0900

    fix: build

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,6 +4,7 @@
     <p class="text-body2">
       Quasar Framework에 대해 알아보겠습니다.<br />열심히 깊게 분석해서 진한 내용을 다루고 싶었지만, 시간관계상 프로젝트 생성 및<br />주요 컴포넌트의 전체적인 흐름을 보는정도로만 준비해보았습니다.<br />급하게 준비하여 미흡 할 수 있습니다. 넓은 마음으로 이해 부탁드리겠습니다.<br />
     </p>
+    <img src="../assets/qrcode.png" class="qrcode"/>
   </q-page>
 </template>
 
