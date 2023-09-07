8e4d0a0d30d7754c044a7b1cbcbd0c6aad70f6ec
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2023-09-07 13:30:54 +0900

    build: build

--- src/pages/EndOfSeminar.vue
+++ src/pages/EndOfSeminar.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="q-pa-md">
     <h4>들어주신 모든 분들 감사드립니다. 고생하셨습니다.</h4>
-    <p class="text-body2">Thank you !</p>
+    <p class="text-body2">Let's Quasar !</p>
   </q-page>
 </template>
 
--- src/pages/build/MainPage.vue
+++ src/pages/build/MainPage.vue
@@ -1,4 +1,10 @@
 <template>
-  <div>build MainPage</div>
+  <div>
+    <h4>빌드와 배포</h4>
+    <ul class="list">
+      <li>간단한 프로젝트 빌드</li>
+      <li>실전 예제와 데모 애플리케이션 소개</li>
+    </ul>
+  </div>
 </template>
 <script lang="ts" setup></script>
